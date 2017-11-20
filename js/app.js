4d8237d5ea82ac5763540a16bdff0e4b6afc2b02
ValeriaNiceria <valerianiceria@gmail.com>
2017-11-20 16:04:43 -0200

    Adicionando o icone de email

--- js/app.js
+++ js/app.js
@@ -17,30 +17,6 @@ debounce = function(func, wait, immediate) {
 };
 
 
-
-$('[data-group]').each(function(){
-    var $allTarget = $(this).find('[data-target]'),
-        $allClick = $(this).find('[data-click]'),
-        activeClass = 'active';
-    
-    $allTarget.first().addClass(activeClass);
-    $allClick.first().addClass(activeClass);
-
-    $allClick.click(function(e){
-        e.preventDefault();
-
-        var id = $(this).data('click'),
-            $target = $('[data-target="' + id +'"]');
-
-        $allClick.removeClass(activeClass);
-        $allTarget.removeClass(activeClass);
-        
-        $target.addClass(activeClass);
-        $(this).addClass(activeClass);
-    });
-}); 
-
-
 //Scroll Suave
 
 $('.menu-nav a[href^="#"]').click(function(e){ //Quando se tem o '^⁼' - quer dizer para selecionar todos com itens que comecem com o que for selecionado
